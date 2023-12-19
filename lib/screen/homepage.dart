f997dd320230e3fafbbe40c6f8e09ffb8bd925af
Mathbech <legeek.you@gmail.com>
2023-12-19 09:31:44 +0100

    Création de la page d'accueil avec compterur de pas

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,22 +1,27 @@
-import 'package:bts_app/main.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'package:pedometer/pedometer.dart';
 
 String formatDate(DateTime d) {
   return d.toString().substring(0, 19);
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
-
 
   static const String routeName = '/';
 
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   late Stream<StepCount> _stepCountStream;
   late Stream<PedestrianStatus> _pedestrianStatusStream;
   String _status = '?', _steps = '?';
 
+  @override
   void initState() {
     super.initState();
     initPlatformState();
@@ -32,22 +37,30 @@ class HomePage extends StatelessWidget {
   }
 
   void onPedestrianStatusChanged(PedestrianStatus event) {
-    print(event);
+    if (kDebugMode) {
+      print(event);
+    }
     setState(() {
       _status = event.status;
     });
   }
 
   void onPedestrianStatusError(error) {
-    print('onPedestrianStatusError: $error');
+    if (kDebugMode) {
+      print('onPedestrianStatusError: $error');
+    }
     setState(() {
       _status = 'Pedestrian Status not available';
     });
-    print(_status);
+    if (kDebugMode) {
+      print(_status);
+    }
   }
 
   void onStepCountError(error) {
-    print('onStepCountError: $error');
+    if (kDebugMode) {
+      print('onStepCountError: $error');
+    }
     setState(() {
       _steps = 'Step Count not available';
     });
@@ -72,8 +85,34 @@ class HomePage extends StatelessWidget {
         backgroundColor: Colors.blue,
         title: const Text('Podomètre'),
       ),
-      body: const Center(
-        child: Text('Podomètre !'),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            const Text('Nombre de pas:'),
+            Text(
+              _steps,
+              style: const TextStyle(fontSize: 30),
+            ),
+            const Text('Activité:'),
+            Icon(
+              _status == 'walking'
+                  ? Icons.directions_walk
+                  : Icons.accessibility_new,
+              size: 50.0,
+            ),
+            Center(
+                child: Text(_status,
+                    style: _status == 'walking'
+                        ? const TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.green,
+                            fontWeight: FontWeight.bold)
+                        : const TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.red,
+                            fontWeight: FontWeight.bold)))
+          ],
+        ),
       ),
     );
   }
